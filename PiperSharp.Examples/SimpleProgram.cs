a02738523b6c29db2116180f4dd319b1529d84a5
Ikars <lyxikars123@gmail.com>
2024-03-09 23:02:21 +0200

    Add simple library tests and examples

---
+++ PiperSharp.Examples/SimpleProgram.cs
@@ -0,0 +1,57 @@
+﻿using PiperSharp.Models;
+
+namespace PiperSharp.Examples;
+
+public class SimpleProgram
+{
+    public static async Task Main()
+             {
+                 var cwd = Directory.GetCurrentDirectory();
+                 var piperPath = Path.Join(cwd, "piper", "piper.exe");
+                 if (!File.Exists(piperPath))
+                 {
+                     await PiperDownloader.DownloadPiper().ExtractPiper(cwd);    
+                 }
+                 var modelPath = Path.Join(cwd, "ar_JO-kareem-low");
+                 VoiceModel? model = null;
+                 if (!Directory.Exists(modelPath))
+                 {
+                     var models = await PiperDownloader.GetHuggingFaceModelList();
+                     model = await models!["ar_JO-kareem-low"].DownloadModel();
+                 }
+                 else
+                 {
+                     model = await VoiceModel.LoadModel(modelPath);
+                 }
+        
+                 var consoleThread = new Thread(ConsoleThread);
+                 
+                 var process = new PiperProvider(new PiperConfiguration()
+                 {
+                     Location = piperPath,
+                     Model = model,
+                     UseCuda = false
+                 });
+                 consoleThread.Start(process);
+                 consoleThread.Join();
+             }
+        
+             public static void ConsoleThread(object? obj)
+             {
+                 var process = (PiperProvider)obj!;
+                 var input = string.Empty;
+                 while (input != "EXIT")
+                 {
+                     Console.Write(">>> ");
+                     input = Console.ReadLine();
+                     if (string.IsNullOrEmpty(input)) continue;
+                     if (input == "EXIT") break;
+                     var data = process.InferAsync(input, AudioOutputType.Wav).GetAwaiter().GetResult();
+                     var fs = File.OpenWrite("audiofile.wav");
+                     fs.Write(data);
+                     fs.Flush();
+                     fs.Close();
+                     Console.WriteLine("File saved to audiofile.wav");
+                 }
+             }
+}
---
+++ PiperSharp.Tests/Tests/PiperSharpTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
+using PiperSharp.Models;
+using NUnit.Framework;
+namespace PiperSharp.Tests.Tests;
+
+[TestFixture]
+public class PiperSharpTests
+{
+    [Test]
+    public async Task TestDownloadPiper()
+    {
+        var cwd = Directory.GetCurrentDirectory();
+        var piperPath = Path.Join(cwd, "piper");
+        if (Directory.Exists(piperPath)) Directory.Delete(piperPath, true);
+        await PiperDownloader.DownloadPiper().ExtractPiper(cwd);
+        Assert.That(Directory.Exists(piperPath), "Piper doesn't exist");
+    }
+    
+    [Test]
+    public async Task TestDownloadModel()
+    {
+        var cwd = Directory.GetCurrentDirectory();
+        const string modelName = "ar_JO-kareem-low";
+        var modelPath = Path.Join(cwd, modelName);
+        if (Directory.Exists(modelPath)) Directory.Delete(modelPath, true);
+        var models = await PiperDownloader.GetHuggingFaceModelList();
+        Assert.That(models is { Count: > 0 }, "Failed to get models from hugging face");
+        var model = models![modelName];
+        Assert.That(model is { Key: modelName }, "Expected model doesn't exist!");
+        model = await model.DownloadModel(cwd);
+        Assert.That(model.ModelLocation == modelPath && Directory.Exists(modelPath), "Model not downloaded!");
+        model = await VoiceModel.LoadModel(modelPath);
+        Assert.That(model is { Key: modelName }, "Failed to load model expected model!");
+    }
+
+    [Test]
+    public async Task TestTTSInference()
+    {
+        var cwd = Directory.GetCurrentDirectory();
+        const string modelName = "ar_JO-kareem-low";
+        var modelPath = Path.Join(cwd, modelName);
+        var piperPath = Path.Join(cwd, "piper", Environment.OSVersion.Platform == PlatformID.Win32NT ? "piper.exe" : "piper");
+        var model = await VoiceModel.LoadModel(modelPath);
+        var piperModel = new PiperProvider(new PiperConfiguration()
+        {
+            Location = piperPath,
+            Model = model,
+        });
+        var result = await piperModel.InferAsync("Hello there!", AudioOutputType.Wav);
+        Assert.That(
+            result[0] == 82 &&
+            result[1] == 73 &&
+            result[2] == 70 &&
+            result[3] == 70,
+            "Expected WAV MAGIC number"
+        );
+        Assert.That(result.Length > 20_000, "Expected larger filesize!");
+    }
+}
